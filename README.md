fb5f3ea6aa27f88e8261a07820e3b37a3aa2a299
Goldslime <kenji188@gmail.com>
2022-03-10 01:37:00 +0800

    a

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # csc2008Flask
+
+# How to run 
+### cd to folder flaskUI
+### 1. <br>flask run 
+### or
+### 2. <br>export FLASK_ENV=development <br>flask run
--- flaskUI/app.py
+++ flaskUI/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__,template_folder="templates")
+IS_DEV = app.env == 'development'
 
 
 @app.route('/')
@@ -9,4 +11,7 @@ def index():
 
 
 if __name__ == 'main':
+     # guaranteed to not be run on a production server
+    assert os.path.exists('.env')  # for other environment variables...
+    os.environ['FLASK_ENV'] = 'development'  # HARD CODE since default is production
     app.run(debug=True)
