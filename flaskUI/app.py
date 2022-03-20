35a1ecd2e541761a9ee1df8fb2840f808bfef640
aloysiuswjw <77778119+aloysiuswjw@users.noreply.github.com>
2022-03-21 02:48:57 +0800

    website graph able to show multi lines and have is a line and the other is a dotted line.

--- flaskUI/app.py
+++ flaskUI/app.py
@@ -9,19 +9,21 @@ IS_DEV = app.env == 'development'
 def index():
     #This is where we import our data from database
     data = [
-        ("2015-Q1", 23.29),
-        ("2015-Q2", 20.87),
-        ("2015-Q3", 22.41),
-        ("2015-Q4", 20.35),
-        ("2016-Q1", 19.5),
-        ("2016-Q2", 17.68),
-        ("2016-Q3", 19.28),
-        ("2016-Q4", 19.13),
-        ("2017-Q1", 20.2),
+        ("2015-Q1", 23.29, 25.6),
+        ("2015-Q2", 20.87, 25.6),
+        ("2015-Q3", 22.41, 25.6),
+        ("2015-Q4", 20.35, 27.9),
+        ("2016-Q1", 19.5, 27.9),
+        ("2016-Q2", 17.68, 27.9),
+        ("2016-Q3", 19.28, 27.9),
+        ("2016-Q4", 19.13, 27.9),
+        ("2017-Q1", 20.2, 24.2),
     ]
+    
     labels = [row[0] for row in data]
-    values = [row[1] for row in data]
-    return render_template("index.html", labels=labels, values=values)
+    electricPrice = [row[1] for row in data]
+    crudePrice = [row[2] for row in data]
+    return render_template("index.html", labels=labels, electricPrice=electricPrice, crudePrice=crudePrice )
 
 
 if __name__ == 'main':
