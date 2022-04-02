ca30a7b8a55be505d80ebf2a807839d943302a1e
aloysiuswjw <77778119+aloysiuswjw@users.noreply.github.com>
2022-04-03 02:22:26 +0800

    added in Maintenance in the dropdownbox and edited on the styles

--- flaskUI/app.py
+++ flaskUI/app.py
@@ -50,22 +50,23 @@ def index():
 
     # This is where we import our data from database
     data = [
-        ("2015-Q1", 23.29, 25.6, 27.4),
-        ("2015-Q2", 20.87, 25.6, 28.8),
-        ("2015-Q3", 22.41, 25.6, 28.7),
-        ("2015-Q4", 20.35, 27.9, 28.1),
-        ("2016-Q1", 19.5, 27.9, 28.4),
-        ("2016-Q2", 17.68, 27.9, 29.1),
-        ("2016-Q3", 19.28, 27.9, 27.7),
-        ("2016-Q4", 19.13, 27.9, 27.1),
-        ("2017-Q1", 20.2, 24.2, 28.3),
+        ("2015-Q1", 23.29, 25.6, 27.4, 21.9),
+        ("2015-Q2", 20.87, 25.6, 28.8, 21.9),
+        ("2015-Q3", 22.41, 25.6, 28.7, 21.9),
+        ("2015-Q4", 20.35, 27.9, 28.1, 21.9),
+        ("2016-Q1", 19.5, 27.9, 28.4, 24.4),
+        ("2016-Q2", 17.68, 27.9, 29.1, 24.4),
+        ("2016-Q3", 19.28, 27.9, 27.7, 24.4),
+        ("2016-Q4", 19.13, 27.9, 27.1, 24.4),
+        ("2017-Q1", 20.2, 24.2, 28.3, 24.9),
     ]
     
     labels = [row[0] for row in data]
     electricPrice = [row[1] for row in data]
     crudePrice = [row[2] for row in data]
     temperature = [row[3] for row in data]
-    return render_template("index.html", labels=labels, electricPrice=electricPrice, crudePrice=crudePrice, temperature=temperature )
+    maintenance = [row[4] for row in data]
+    return render_template("index.html", labels=labels, electricPrice=electricPrice, crudePrice=crudePrice, temperature=temperature, maintenance=maintenance )
 
 
 if __name__ == 'main':
