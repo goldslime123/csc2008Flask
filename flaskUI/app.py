e2db524307a8aaf236f1db3ebad5e7cf724639d4
Goldslime <kenji188@gmail.com>
2022-04-04 00:56:58 +0800

    merge

--- flaskUI/app.py
+++ flaskUI/app.py
@@ -3,11 +3,12 @@ import os
 
 # postgres
 import psycopg2
+# MariaDB Imports
+import mariadb
 import sys
 
 app = Flask(__name__, template_folder="Website")
 IS_DEV = app.env == 'development'
-
 
 @app.route('/')
 def index():
@@ -16,13 +17,14 @@ def index():
     conn = None
 
     # Connect to postgresql Platform
-    try:        
+    try:
         conn = psycopg2.connect(
             host="ec2-54-173-77-184.compute-1.amazonaws.com",
             database="d2v75ijfptfl5f",
             user="jkbetvbzvsivpk",
-            password="3b79c1f6062e3164cb523ea49ade123ccc4d25a86f7fa9c7e2b42921d0f55831") 
-        
+            password=
+            "3b79c1f6062e3164cb523ea49ade123ccc4d25a86f7fa9c7e2b42921d0f55831")
+
         print("Successfully connected", file=sys.stderr)
 
     except Exception as error:
@@ -30,16 +32,17 @@ def index():
 
     # Get Cursor
     if conn is not None:
-    
+
         cur = conn.cursor()
         cur.execute(
-        "SELECT w.quarter, w.temperature, t.tariff_per_kwh, c.price_per_barrel, m.cost FROM weather w, tariff t, crudeoil c, maintenance m WHERE w.quarter=c.quarter and  w.quarter=t.quarter and m.quarter=w.quarter;")
-
+            "SELECT w.quarter, w.temperature, t.tariff_per_kwh, c.price_per_barrel, m.cost FROM weather w, tariff t, crudeoil c, maintenance m WHERE w.quarter=c.quarter and  w.quarter=t.quarter and m.quarter=w.quarter;"
+        )
 
         for i in cur:
             list.append(i)
 
-        data = [(item[0], float(item[1]), float(item[2]), float(item[3]), float(item[4])) for item in list]
+        data = [(item[0], float(item[1]), float(item[2]), float(item[3]),
+                 float(item[4])) for item in list]
         for i in data:
             print(i, file=sys.stderr)
 
@@ -49,8 +52,12 @@ def index():
         electricPrice = [row[2] for row in data]
         crudePrice = [row[3] for row in data]
         maintenance = [row[4] for row in data]
-        return render_template("index.html", labels=labels, electricPrice=electricPrice, crudePrice=crudePrice,temperature=temperature, maintenance=maintenance )
-
+        return render_template("index.html",
+                               labels=labels,
+                               electricPrice=electricPrice,
+                               crudePrice=crudePrice,
+                               temperature=temperature,
+                               maintenance=maintenance)
 
     # # This is where we import our data from database
     # data = [
@@ -64,7 +71,7 @@ def index():
     #     ("2016-Q4", 19.13, 27.9, 27.1, 24.4),
     #     ("2017-Q1", 20.2, 24.2, 28.3, 24.9),
     # ]
-    
+
     # labels = [row[0] for row in data]
     # electricPrice = [row[1] for row in data]
     # crudePrice = [row[2] for row in data]
