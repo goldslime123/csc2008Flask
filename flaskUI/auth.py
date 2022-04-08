3e7adbf3aac5e541c3f90bc31607de9c600b6f0b
Goldslime <kenji188@gmail.com>
2022-04-08 22:54:23 +0800

    r

---
+++ flaskUI/auth.py
@@ -0,0 +1,7 @@
+import psycopg2
+connMongo = 'mongodb://user:password@notjyzh.duckdns.org:27017/?authSource=test'
+
+host="ec2-54-173-77-184.compute-1.amazonaws.com"
+database="d2v75ijfptfl5f"
+user="jkbetvbzvsivpk"
+password="3b79c1f6062e3164cb523ea49ade123ccc4d25a86f7fa9c7e2b42921d0f55831"
